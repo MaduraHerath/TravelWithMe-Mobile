f725e89315cd891e91ea1d6de376b35ac563bbda
Madura Herath <maduraherath8@gmail.com>
2017-07-24 15:21:45 +0530

    add mlab db

--- newclone/TravelwithMe-master/config/database.js
+++ newclone/TravelwithMe-master/config/database.js
@@ -4,6 +4,6 @@
 
 module.exports = {
 
-    database:"mongodb://127.0.0.1:27017/travelbook",
+    database:["mongodb://127.0.0.1:27017/travelbook","mongodb://user:user123@ds035036.mlab.com:35036/travelwithme"],
     secret:"travelbooksecretkey"
 };
